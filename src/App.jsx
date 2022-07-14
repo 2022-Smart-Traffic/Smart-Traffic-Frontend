4aa52d35c067c0926da4eb0a823c82fcca156d98
lilunderdog <simonha3941@gmail.com>
2022-07-14 12:01:14 +0900

    header , kakao map css 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainComponent } from './components/MainComponent';
-import Map  from './components/Map';
+import Map from './components/Map';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-            <Routes>
-                <Route path={"/map"} element={
-                  <div>
-                    <Header />
-                    <Map />
-                  </div>
-                  }></Route>
-                <Route path={"/"} element={<MainComponent />}></Route>
-            </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/map"} element={
+            <div style={{ maxWidth: "1280px", width: "100vw", height: "100vh", }}>
+              <Header />
+              <Map />
+            </div>
+          }></Route>
+          <Route path={"/"} element={<MainComponent />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
