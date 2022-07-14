26208b331de88fcade227e772f30e3029ee90053
dlatldhs <sionzz1713@gmail.com>
2022-07-14 19:56:32 +0900

    메인페이지 디자인 수정

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -2,25 +2,26 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import '../styles/main.css';
 import { ReactComponent as BusIcon } from '../svg/bus.svg';
-
+import Header from "./Header";
 export const MainComponent = () => {
     return (
         <div className="main">
             <div className="main--quick-menu">
-                <Link to="/bus">
+                <Header />
+                <Link to="/map">
                     <div className="main--quick-menu-icon-wrap">
                         <BusIcon className="main--quick-menu-icon" />
                     </div>
                     <span>버스</span>
                 </Link>
                 <Link to="/subway">
-                    <div className="main--quick-menu-icon-wrap">
+                    <div id="subway" className="main--quick-menu-icon-wrap">
                         <BusIcon className="main--quick-menu-icon" />
                     </div>
                     <span>지하철</span>
                 </Link>
                 <Link to="/taxi">
-                    <div className="main--quick-menu-icon-wrap">
+                    <div id="taxi" className="main--quick-menu-icon-wrap">
                         <BusIcon className="main--quick-menu-icon" />
                     </div>
                     <span>택시</span>
